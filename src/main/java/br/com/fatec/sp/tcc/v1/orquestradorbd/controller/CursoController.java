5704cff736e94854dc7221e7b88afc7d2a055f5f
Wellington <wellingtoncarvalho11@gmail.com>
2021-09-02 21:59:31 -0300

    feat: implementacao get curso

--- src/main/java/br/com/fatec/sp/tcc/v1/orquestradorbd/controller/CursoController.java
+++ src/main/java/br/com/fatec/sp/tcc/v1/orquestradorbd/controller/CursoController.java
@@ -2,10 +2,31 @@ package br.com.fatec.sp.tcc.v1.orquestradorbd.controller;
 
 import br.com.fatec.sp.tcc.v1.orquestradorbd.config.AbstractController;
 import br.com.fatec.sp.tcc.v1.orquestradorbd.config.SaidaDefault;
+import br.com.fatec.sp.tcc.v1.orquestradorbd.controller.response.CursoResponse;
+import br.com.fatec.sp.tcc.v1.orquestradorbd.facade.CursoFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/curso")
 public class CursoController implements AbstractController<SaidaDefault> {
+
+    @Autowired
+    private CursoFacade cursoFacade;
+
+
+    @GetMapping
+    public ResponseEntity<?> getCursos(){
+
+        List<CursoResponse> cursos = cursoFacade.getCursos();
+
+        return saidaSimplificada(SaidaDefault.builder().responseBody(cursos).message("Lista Retornada com Sucesso").build(), HttpStatus.OK);
+
+    };
 }
